7d1255fe8108e7986f9f5b505ca54320893658e3
alonso-estevam <alonso_estevam@outlook.com>
2023-03-02 12:54:35 -0300

    refatora método de padronizacao dos campos numéricos

--- src/br/com/digytalspace/contarural/util/PadroesBacen.java
+++ src/br/com/digytalspace/contarural/util/PadroesBacen.java
@@ -2,46 +2,41 @@ package br.com.digytalspace.contarural.util;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class PadroesBacen {
 	String campoPadronizado = "";
 	
 	/**
 	 * Padroniza nomes em geral. Se o valor for superior ao tamanho máximo de caracteres,
-	 * corta o final do nome; caso seja inferior, completa com espaços à esquerda. Aplica-se a:
-	 * nome (30),
-	 * logradouro (60),
-	 * bairro (30),
-	 * complemento (20),
-	 * cidade (50)
-	 * @param campo
-	 * @param tamanhoMaximoDeCaracteres
+	 * corta o final do nome; caso seja inferior, completa com espaços à esquerda. 
+	 * <p>Aplica-se a:
+	 * 	<ul>
+	 * 		<li>nome (30)</li>
+	 * 		<li>logradouro (60)</li>
+	 * 		<li>bairro (30)</li>
+	 * 		<li>complemento (20)</li>
+	 * 		<li>cidade (50)</li>
+	 * 	</ul>
+	 * </p>
+	 * @param dado
+	 * @param tamanhoMaximo
 	 * @return String representando textos em geral padronizados conforme BACEN
 	 */
-	public String padronizaString(String campo, Integer tamanhoMaximoDeCaracteres){
-		if(campo.length() > tamanhoMaximoDeCaracteres){
-			campoPadronizado = campo.substring(0, tamanhoMaximoDeCaracteres);
-		} 
-		
-		if(campo.length() < tamanhoMaximoDeCaracteres) {
-			StringBuilder builder = new StringBuilder();
-			for(int i = 0; i < tamanhoMaximoDeCaracteres; i++) {
-				builder.append(" ");
-			}
-			campoPadronizado = builder.insert(builder.length() - campo.length(), campo).toString().stripTrailing();
+	public String padronizaDadosTextuais(String dado, Integer tamanhoMaximo){
+		if(dado.length() > tamanhoMaximo){
+			return dado.substring(0, tamanhoMaximo).toUpperCase();
+		} else {
+			return preencheCampo(dado, tamanhoMaximo, ' ').toUpperCase();					
 		}
-		
-		return campoPadronizado.toUpperCase();
 	}
+	
 	/**
 	 * Padroniza datas, retirando quaisquer caracteres que não sejam números
 	 * e colocando nesta ordem: ano, mês, dia.
 	 * @param data
 	 * @return String representando a data padronizada conforme BACEN
 	 */
-	public String padronizaData(String data) {
+	public String padronizaDatas(String data) {
 		String[] numerosData = data.split("/");
 		Integer dia = Integer.parseInt(numerosData[0]);
 		Integer mes = Integer.parseInt(numerosData[1]);
@@ -52,20 +47,43 @@ public class PadroesBacen {
 		return dataPadronizada.format(dateTimeFormatter);
 	}
 	
-	public String padronizaRendaAtual(String rendaAtual) {
-		String rendaPadronizada = "";
-		if(rendaAtual == null || rendaAtual == "") {
-			rendaPadronizada = "0000000000";
-		}
-		String semPonto = rendaAtual.replace(".", "");
-		String semVirgula = semPonto.replace(",", "");
-		StringBuilder builder = new StringBuilder();
-		for(int i = 0; i < 10 - semVirgula.length(); i++) {
-			builder.append("0");
-		}
-		rendaPadronizada = builder.append(semVirgula).toString();
-		return rendaPadronizada;
+	/***
+	 * Padroniza strings de números, retirando caracteres como ponto, vírgula, traços etc.
+	 * Deixa somente os números e preenche o espaço restante com 0 (zero). 
+	 * <p>Aplica-se a:
+	 * 	<ul>
+	 * 		<li>renda atual (10)</li>
+	 * 		<li>telefone (10)</li>
+	 * 		<li>celular (11)</li>
+	 * 		<li>número (5)</li>
+	 * 		<li>cep (8)</li>
+	 * 	</ul>
+	 * </p> 
+	 * @param dado
+	 * @param tamanhoMaximo
+	 * @return
+	 */
+	public String padronizaDadosNumericos(String dado, int tamanhoMaximo) {
+		String dadoFiltrado = filtraCaracteresNaoNumericos(dado);
+		return preencheCampo(dadoFiltrado, tamanhoMaximo, '0');
 	}
 	
+	private String preencheCampo(String dado, int tamanhoMaximo, char caractere) {
+		if(tamanhoMaximo < dado.length()) {
+			throw new IllegalArgumentException();
+		}
+		
+		StringBuilder sb = new StringBuilder(dado);
+		
+		for(int i = 0; i < tamanhoMaximo - dado.length(); i++) {
+			sb.insert(0, caractere);
+		}
+		
+		return sb.toString();
+	}
+	
+	private String filtraCaracteresNaoNumericos(String dado) {
+		return dado.replaceAll("[^0-9]", "");
+	}
 	
 }
